70a396bf79944fc5d3f581f48a071081a9feea4b
snehalupatil <patilsnehal4575@gmail.com>
2021-05-23 14:42:22 +0530

    Added: Create a BST by adding Multiple nodes

--- test/java/MyBinaryTreeTest.java
+++ test/java/MyBinaryTreeTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class MyBinaryTreeTest {
 
     @Test
-    public void givenNumberWhenAddedBinaryTreeShouldReturnSizeThree(){
+    public void givenNumber_WhenAddedBinaryTree_ShouldReturnSizeThree(){
         MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
@@ -12,5 +12,26 @@ public class MyBinaryTreeTest {
 
         int size =myBinaryTree.getSize();
         Assertions.assertEquals(3,size);
+    }
+
+    @Test
+    public void givenNumber_WhenAddedBinaryTree_ShouldReturnSize13(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+
+        int size =myBinaryTree.getSize();
+        Assertions.assertEquals(13,size);
     }
 }
