ba65a7722afdc5ce40f1dbefe3d1fdee0b03c50b
snehalupatil <patilsnehal4575@gmail.com>
2021-05-23 14:45:38 +0530

    Added: Create a BST by adding Multiple nodes

--- test/java/MyBinaryTreeTest.java
+++ test/java/MyBinaryTreeTest.java
@@ -13,4 +13,25 @@ public class MyBinaryTreeTest {
         int size =myBinaryTree.getSize();
         Assertions.assertEquals(3,size);
     }
+
+    @Test
+    public void givenNumberWhenAddedBinaryTreeShouldReturnSize13(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+
+        int size =myBinaryTree.getSize();
+        Assertions.assertEquals(13,size);
+    }
 }
